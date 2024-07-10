00e7948be6d52346cf069d06368e23cff6c0597e
raffi23 <raffi@rhymecode.net>
2024-07-10 14:19:53 +0400

    add google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePluginRadar({
+      analytics: { id: "G-TZG2JYBB48" },
+    }),
+  ],
 });
