5b00dfe6aef659c45d3d6c06280a82452e5bc99b
raffi23 <raffi@rhymecode.net>
2024-07-10 12:41:50 +0400

    chore: reduce icons image sizes

--- src/animations/Demo-08/helpers/constants.ts
+++ src/animations/Demo-08/helpers/constants.ts
@@ -4,53 +4,61 @@ export const homeIcons: App[] = [
   {
     id: "com.rhymecode.messages",
     title: "Messages",
-    imageUrl: "/demo-08/messages.png",
+    imageUrl: "/demo-08/messages.webp",
   },
-  { id: "com.rhymecode.maps", title: "Maps", imageUrl: "/demo-08/maps.png" },
+  { id: "com.rhymecode.maps", title: "Maps", imageUrl: "/demo-08/maps.webp" },
   {
     id: "com.rhymecode.photos",
     title: "Photos",
-    imageUrl: "/demo-08/photos.png",
+    imageUrl: "/demo-08/photos.webp",
   },
   {
     id: "com.rhymecode.camera",
     title: "Camera",
-    imageUrl: "/demo-08/camera.png",
+    imageUrl: "/demo-08/camera.webp",
   },
   {
     id: "com.rhymecode.contacts",
     title: "Contacts",
-    imageUrl: "/demo-08/contacts.png",
+    imageUrl: "/demo-08/contacts.webp",
   },
-  { id: "com.rhymecode.notes", title: "Notes", imageUrl: "/demo-08/notes.png" },
+  {
+    id: "com.rhymecode.notes",
+    title: "Notes",
+    imageUrl: "/demo-08/notes.webp",
+  },
   {
     id: "com.rhymecode.calendar",
     title: "Calendar",
-    imageUrl: "/demo-08/calendar.png",
+    imageUrl: "/demo-08/calendar.webp",
   },
   {
     id: "com.rhymecode.wallet",
     title: "Wallet",
-    imageUrl: "/demo-08/wallet.png",
+    imageUrl: "/demo-08/wallet.webp",
   },
   {
     id: "com.rhymecode.settings",
     title: "Settings",
-    imageUrl: "/demo-08/settings.png",
+    imageUrl: "/demo-08/settings.webp",
   },
 ];
 
 export const dockIcons: App[] = [
-  { id: "com.rhymecode.phone", title: "Phone", imageUrl: "/demo-08/phone.png" },
-  { id: "com.rhymecode.mail", title: "Mail", imageUrl: "/demo-08/mail.png" },
+  {
+    id: "com.rhymecode.phone",
+    title: "Phone",
+    imageUrl: "/demo-08/phone.webp",
+  },
+  { id: "com.rhymecode.mail", title: "Mail", imageUrl: "/demo-08/mail.webp" },
   {
     id: "com.rhymecode.safari",
     title: "Safari",
-    imageUrl: "/demo-08/safari.png",
+    imageUrl: "/demo-08/safari.webp",
   },
   {
     id: "com.rhymecode.calculator",
     title: "Calculator",
-    imageUrl: "/demo-08/calculator.png",
+    imageUrl: "/demo-08/calculator.webp",
   },
 ];
